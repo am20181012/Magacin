4ac174acd859c343cdc4942d6acf25d5b48e8c0b
am20181012 <andjelamijatovic98@gmail.com>
2021-03-13 13:59:07 +0100

    Dodata implementacija metoda iz interfejsa IMagacin u klasi MagacinImpl

--- src/impl/MagacinImpl.java
+++ src/impl/MagacinImpl.java
@@ -1,23 +1,53 @@
 package impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import magacin.Artikal;
 import specifikacija.IMagacin;
 
 public class MagacinImpl implements IMagacin {
+	
+	private List<Artikal> artikli;
+
+	public MagacinImpl() {
+		artikli = new ArrayList<>();
+	}
 
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		
+		if(artikli.contains(artikal)) {
+			for(int i = 0; i < artikli.size(); i++) {
+				if(artikli.get(i).equals(artikal)) {
+					artikli.get(i).setKolicina(artikli.get(i).getKolicina() + 1);
+				}
+			}
+		} else {
+			artikli.add(artikal);
+		}
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		
+		if(artikal == null)
+			throw new NullPointerException("Uneti artikal ne moze biti null.");
+		if(!artikli.contains(artikal))
+			throw new RuntimeException("Uneti artikal se ne nalazi na listi artikla.");
+		artikli.remove(artikal);
 	}
 
 	@Override
 	public Artikal nadji(int sifra) {
-		return null;
+		if(sifra == 0 || sifra < 0) {
+			throw new RuntimeException("Sifra mora biti pozitivan broj");
+		} else {
+			for(Artikal a: artikli) {
+				if(a.getSifra() == sifra)
+					return a;
+			}
+			throw new RuntimeException("U lisiti nema artikla pod navedenom sifrom.");
+		}
+		
 	}
 	
 }
