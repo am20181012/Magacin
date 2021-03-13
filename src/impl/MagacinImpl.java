251421d9f42d899752948483c6d0ed8f09952c60
am20181012 <andjelamijatovic98@gmail.com>
2021-03-13 13:44:01 +0100

    Implementirana logika svih metoda klase MagacinImpl

--- src/impl/MagacinImpl.java
+++ src/impl/MagacinImpl.java
@@ -1,22 +1,59 @@
 package impl;
 
+import java.util.List;
+
 import magacin.Artikal;
 import specifikacija.IMagacin;
 
 public class MagacinImpl implements IMagacin {
+	
+	private List<Artikal> artikli;
+	
+	public MagacinImpl(List<Artikal> artikli) {
+		super();
+		this.artikli = artikli;
+	}
+
+	public List<Artikal> getArtikli() {
+		return artikli;
+	}
+
+	public void setArtikli(List<Artikal> artikli) {
+		this.artikli = artikli;
+	}
 
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		
+		if(artikal == null) {
+			throw new NullPointerException("Uneti artikal ne moze biti null.");
+		}
+		if(artikli.contains(artikal)) {
+			int i = artikli.indexOf(artikal);
+			int kolicina = artikli.get(i).getKolicina();
+			artikli.get(i).setKolicina(kolicina + 1);
+		} else {
+			artikli.add(artikal);
+		}
+			
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		
+		if(artikal == null) {
+			throw new NullPointerException("Uneti artikal ne moze biti null.");
+		}
+		artikli.remove(artikal);
 	}
 
 	@Override
 	public Artikal nadji(int sifra) {
+		if(sifra == 0 || sifra < 0) {
+			throw new RuntimeException("Uneta sifra nije ispravna.");
+		}
+		for(Artikal a: artikli) {
+			if(a.getSifra() == sifra)
+				return a;
+		}
 		return null;
 	}
 	
