b86d34fb36571f317fea0fbc50a63514a242a727
am20181012 <andjelamijatovic98@gmail.com>
2021-03-13 13:02:32 +0100

    Kreirana klasa MagacinImpl koja ce implementirati i dati logiku metoda interfejsa IMagacin

---
+++ src/impl/MagacinImpl.java
@@ -0,0 +1,23 @@
+package impl;
+
+import magacin.Artikal;
+import specifikacija.IMagacin;
+
+public class MagacinImpl implements IMagacin {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		
+	}
+
+	@Override
+	public Artikal nadji(int sifra) {
+		return null;
+	}
+	
+}
